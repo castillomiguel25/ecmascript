e67769a8bdb96e292c836e56c7ff50bcfed3b74f
castillomiguel25 <miguelangelcastillo947@gmail.com>
2022-09-17 18:07:28 -0400

    readme

---
+++ src/Readme.md
@@ -0,0 +1,53 @@
+JavaScript
+
+Lenguaje de programación que utiliza esta especificación (ECMAScript), para trabajar con estas características que van siendo agregadas anualmente a partir del año 2015, en el cual se lanzó la versión 6.Estuvo sin sacar ninguna nueva version (16 años, 1996 - 2001)
+
+ECMAScript
+
+ECMAScript es la especificación del lenguaje propuesto por ECAM internacional una institución encargada de los estándares. JS es el lenguaje que esta especificación usa para trabajar sobre las características que son añadidas año con año. Iniciando en el 2015 con la versión 6.
+
+
+¿Qué se implementó en ES6?
+
+- Default params
+    
+    ```jsx
+    functionmyFunction(param1 = 'default', param2) { ... }
+    ```
+    
+- Concatenación
+    
+    ```jsx
+    let hello = 'welcome';
+    let world = 'to the jungle!';
+    let guns = `${hello} ${world}` // Welcome to the jungle!
+    ```
+
+    Let, const, multilínea, spread and destructuration
+
+    ```jsx
+let variable1; // Block scope
+const constant1; // Block scope and constant
+const stringmultiline = `this is a multi
+line string`;
+
+const array = ['item1', 'item2'];
+const array2 = ['item3', 'item4'];
+const allArrays = [...array, ...array2] // ['item1', 'item2' , 'item3', 'item4']
+
+const object = {
+	name: 'manuel',
+	age: 24,
+	country: 'PE',
+};
+
+const { name, age } = object; // 'manuel', 24
+```
+
+## Arrow Functions, Promesas y Parámetros en objetos
+
+- composicion - `newPromise()`
+    - `resolve()` - comportamiento al cumplir la promesa
+    - `reject()` - comportamiento al fallar la promesa
+    - `.then (⇒)` - obtener el valor del resolve
+    - `.error(⇒)` - obtener el valor del reject()
--- src/es11/index.js
+++ src/es11/index.js
@@ -1,6 +1,66 @@
 const button = document.getElementById("btn");
 
-button.addEventListener("click", async function(){
+button.addEventListener("click", async function () {
     const module = await import("./file.js");
     module.hello();
 });
+
+
+// BigInt permite trabajar con numeros mayores a 2^53
+// 1- Añadiendo una "n" al final activas el big int en el valor
+
+const aBigNumber = 9007199254740991n;
+
+const anotherBigNumber = BigInt(9007199254740991);
+
+console.log(aBigNumber);
+console.log(anotherBigNumber);
+
+// 🤝 Promise All Settled
+// El Promise.allSettled()método devuelve una promesa que se cumple
+//  después de que todas las promesas dadas 
+// se hayan cumplido o rechazado, con una matriz de 
+// objetos que describen el resultado de cada promesa.
+// Por lo general, se usa cuando tiene varias tareas 
+// asincrónicas que no dependen unas de otras para completarse correctamente,
+//  o si siempre desea saber el resultado de cada promesa.
+
+const promise1 = new Promise((resolve, reject) => reject("reject"));
+const promise2 = new Promise((resolve, reject) => resolve("resolve"));
+const promise3 = new Promise((resolve, reject) => resolve("resolve 1"));
+
+Promise.allSettled([promise1, promise2, promise3])
+    .then(response => console.log(response));
+
+// 🌎 Global This
+
+console.log(window);
+console.log(globalThis);
+
+
+// 🔍 Nuevo operador lógico: null operator
+const fooo = null ?? 'default string';
+console.log(fooo);
+
+
+const fooo2 = 'not null' ?? 'default string';
+console.log(fooo2)
+
+// ⛓ Optional chaining
+// permite leer el valor de una propiedad ubicada dentro de una cadena de objetos
+//  conectados sin tener que validar expresamente que cada referencia en la cadena sea válida.
+//   El operador ?. funciona de manera similar a el operador de encadenamiento .,
+//    excepto que en lugar de causar un error si una referencia es casi-nula (null o undefined), 
+// la expresión hace una evaluación de circuito corto con un valor de retorno de undefined.
+
+
+const user = {};
+console.log(user?.profile?.email);
+
+if(user?.profile?.email) {
+    console.log('email')
+} else{
+    console.log('faile')
+}
+
+
